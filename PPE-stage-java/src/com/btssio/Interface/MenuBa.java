9f2d092b20a9ee0938f056ec7dabfecc92b5d8d8
Victor Dittiere <victor.dittiere@icloud.com>
2018-04-24 10:14:25 +0200

    Correction

--- PPE-stage-java/src/com/btssio/Interface/MenuBa.java
+++ PPE-stage-java/src/com/btssio/Interface/MenuBa.java
@@ -8,12 +8,14 @@ import com.btssio.controller.Cmenu;
 
 public class MenuBa extends JMenuBar{
 
+	//Menu
 	private JMenu m1 = new JMenu("Fichier");
 	private JMenu m2 = new JMenu("Eleve");
 	private JMenu m3 = new JMenu("Entreprise");
-	
+	private JMenu m4 = new JMenu("Configuration");
 	private Cmenu cmenu;
-	
+
+	//MenuItem
 	private JMenuItem menuItemListEleve = new JMenuItem("Lister");
 	private JMenuItem menuItemEditEleve = new JMenuItem("Editer");
 	private JMenuItem menuItemCreateEleve = new JMenuItem("Creer");
@@ -22,7 +24,6 @@ public class MenuBa extends JMenuBar{
 	private JMenuItem menuItemCreateEntr = new JMenuItem("Creer");
 	private JMenuItem menuItemAccueil = new JMenuItem("Accueil");
 	private JMenuItem menuItemQuit = new JMenuItem("Quitter");
-	private JMenuItem menuItemConf = new JMenuItem("Configuration");
 
 	
 	public MenuBa() {
@@ -36,9 +37,6 @@ public class MenuBa extends JMenuBar{
 		//m1
 		this.m1.add(menuItemAccueil);
 		this.menuItemAccueil.setActionCommand("Accueil");
-		
-		this.m1.add(menuItemConf);
-		this.menuItemConf.setActionCommand("Configuration");
 		
 		this.m1.add(menuItemQuit);
 		this.menuItemQuit.setActionCommand("Quitter");
@@ -63,33 +61,30 @@ public class MenuBa extends JMenuBar{
 		this.m3.add(menuItemCreateEntr);
 		this.menuItemCreateEntr.setActionCommand("Create entreprise");
 		
-		this.m3.add(menuItemCreateEntr);
-		this.menuItemCreateEntr.setActionCommand("Create entreprise");
-		
 		//Menu add action listener
-
 		//m1
-
 		this.menuItemAccueil.addActionListener(cmenu);
 		this.menuItemQuit.addActionListener(cmenu);
 		
 		//m2
-
 		this.menuItemListEleve.addActionListener(cmenu);
 		this.menuItemEditEleve.addActionListener(cmenu);
 		this.menuItemCreateEleve.addActionListener(cmenu);
 
 		//m3
-
 		this.menuItemListEntr.addActionListener(cmenu);
 		this.menuItemEditEntr.addActionListener(cmenu);
 		this.menuItemCreateEntr.addActionListener(cmenu);
-
 		this.menuItemCreateEntr.addActionListener(new Cmenu(this));
+		
+		//m4
+		this.m4.setText("Configuration");
+		this.m4.addActionListener(new Cmenu(this));
 
-			//Ajout label au menu
+		//Ajout label au menu
 		this.add(this.m1);
 		this.add(this.m2);
 		this.add(this.m3);
+		this.add(this.m4);
 	}
 }
