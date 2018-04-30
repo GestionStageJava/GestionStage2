8270cf52544722357f7fbc92db0b21b5c5d70003
YukiNoKumo <lurnt@protonmail.com>
2018-04-30 14:12:16 +0200

    Stage all modif

--- PPE-stage-java/src/com/btssio/Interface/MenuBa.java
+++ PPE-stage-java/src/com/btssio/Interface/MenuBa.java
@@ -63,13 +63,7 @@ public class MenuBa extends JMenuBar{
 		
 		this.m3.add(menuItemCreateEntr);
 		this.menuItemCreateEntr.setActionCommand("Create entreprise");
-		
-<<<<<<< Updated upstream
-=======
-		this.m3.add(menuItemCreateEntr);
-		this.menuItemCreateEntr.setActionCommand("Create entreprise");
-		
->>>>>>> Stashed changes
+
 		//Menu add action listener
 		//m1
 		this.menuItemAccueil.addActionListener(cmenu);
@@ -84,13 +78,7 @@ public class MenuBa extends JMenuBar{
 		//m3
 		this.menuItemListEntr.addActionListener(cmenu);
 		this.menuItemEditEntr.addActionListener(cmenu);
-<<<<<<< Updated upstream
 		this.menuItemCreateEntr.addActionListener(cmenu);
-		this.menuItemCreateEntr.addActionListener(new Cmenu(this));
-=======
-		this.menuItemCreateEntr.addActionListener(cmenu);
-
->>>>>>> Stashed changes
 
 		//Ajout label au menu
 		this.add(this.m1);
